d9e8f72970bff114dd96f4d432136fc00f9ccf95
l1fk0t <s.uguccioni@countermail.com>
2013-10-27 05:30:38 +0100

    final test

--- lilybuzz/lilybuzz.ino
+++ lilybuzz/lilybuzz.ino
@@ -1,8 +1,4 @@
-//#define DEBUG 0
-
-#define bluetoothTx 2
-#define bluetoothRx 3
-
+#define DEBUG
 #define redPin 9
 #define greenPin 5
 #define bluePin 6
@@ -23,83 +19,74 @@ void setup(){
   pinMode(wled2Pin, OUTPUT);
   pinMode(pledPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
-  pinMode(redPin, OUTPUT); 
-  pinMode(greenPin, OUTPUT); 
-  pinMode(bluePin, OUTPUT);
+  //pinMode(redPin, OUTPUT); 
+  //pinMode(greenPin, OUTPUT); 
+  //pinMode(bluePin, OUTPUT);
 
-  
+
   Serial.begin(9600);  // Begin the serial monitor at 9600bps
   delay(100);
-  
+
   off();
 }
 
-  
+
 void loop(){
-  
+
   if (Serial.available()){ //if bluetooth sent any characters
     // send ay character the bluetooth prints to the serial monitor
     int intValue = (int)Serial.read();
-  #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("Received integer value: ");
     Serial.println(intValue);
-  #endif
-  }
-  
-  #ifdef DEBUG
-    intValue = (loop_count % 10) + 1;
-    Serial.print("Received integer value: ");
-    Serial.println(intValue);
-  #endif
-  
-  switch (intValue){
+#endif
+
+    switch (intValue){
     case 1: // connection ON
       rgb_green();
       break;
     case 2:  // notification
       buzz_rgb_blue();
       break;
-     case 3: // system error
-       rgb_red();
-       break;
-     case 4: // all off
-       off();
-       break;
-     case 5:  // go straight
-       digitalWrite(wled1Pin, HIGH);
-       digitalWrite(wled2Pin, HIGH);
-       break;
-     case 6: // turn left
-       digitalWrite(bledPin, LOW);
-       digitalWrite(wled1Pin, LOW);
-       digitalWrite(wled2Pin, HIGH);
-       digitalWrite(pledPin, HIGH);
-       break;
-     case 7: // turn right
-       digitalWrite(bledPin, HIGH);
-       digitalWrite(wled1Pin, HIGH);
-       digitalWrite(wled2Pin, LOW);
-       digitalWrite(pledPin, LOW);
-       break;
-     case 8: // wrong path
-       for (int i=0; i<10; i++){
-         flicker_blue_pink();
-       }
-       break;
-     case 9: //arrived
-       all_leds_flash();
-       off();
-       break;
-     case 10: // navigation off
-       nav_off();
-       break;
+    case 3: // system error
+      rgb_red();
+      break;
+    case 4: // all off
+      off();
+      break;
+    case 5:  // go straight
+      digitalWrite(wled1Pin, HIGH);
+      digitalWrite(wled2Pin, HIGH);
+      break;
+    case 6: // turn left
+      digitalWrite(bledPin, LOW);
+      digitalWrite(wled1Pin, LOW);
+      digitalWrite(wled2Pin, HIGH);
+      digitalWrite(pledPin, HIGH);
+      break;
+    case 7: // turn right
+      digitalWrite(bledPin, HIGH);
+      digitalWrite(wled1Pin, HIGH);
+      digitalWrite(wled2Pin, LOW);
+      digitalWrite(pledPin, LOW);
+      break;
+    case 8: // wrong path
+      for (int i=0; i<10; i++){
+        flicker_blue_pink();
+      }
+      break;
+    case 9: //arrived
+      all_leds_flash();
+      off();
+      break;
+    case 10: // navigation off
+      nav_off();
+      break;
+    }
   }
-  
-  #ifdef DEBUG
-    ++loop_count;
-    delay(11000);
-  #endif
-  
+
+
+
 }
 
 void nav_off(){
@@ -127,7 +114,7 @@ void all_leds_flash(){
 void flicker_blue_pink(){
   digitalWrite(wled1Pin, LOW);
   digitalWrite(wled2Pin, LOW);
-  
+
   digitalWrite(bledPin, HIGH);
   digitalWrite(pledPin, HIGH);
   delay(300);
@@ -139,8 +126,8 @@ void off(){
   nav_off();
   rgb_off();
 }
-  
-  
+
+
 void beep(int frequencyInHertz, long timeInMilliseconds)
 {
   int x;
@@ -160,17 +147,17 @@ void buzz_rgb_blue()
 {
   int bkp_rgb = rgb_value;
   beep(2093,500);	//C: play the note C (C7 from the chart linked to above)
-  
+
   switch (bkp_rgb){
-    case 0:
-      rgb_red();
-      break;
-    case 1:
-      rgb_green();
-      break;
-    case 2:
-      rgb_blue();
-      break;
+  case 0:
+    rgb_red();
+    break;
+  case 1:
+    rgb_green();
+    break;
+  case 2:
+    rgb_blue();
+    break;
   }
 }
 
@@ -196,8 +183,9 @@ void rgb_off(){
 
 void color (unsigned char red, unsigned char green, unsigned char blue) // the color generating function
 {	 
-  analogWrite(redPin, 255-red);	 
-  analogWrite(bluePin, 255-blue);
-  analogWrite(greenPin, 255-green);
+  //analogWrite(redPin, 255-red);	 
+  //analogWrite(bluePin, 255-blue);
+  //analogWrite(greenPin, 255-green);
 }	 
+
 
